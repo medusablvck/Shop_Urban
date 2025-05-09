a9f9a1edc0e3bdb2e7615529afae1dc7859a949f
medusablvck <e37207169@gmail.com>
2025-05-09 16:33:17 +0300

    add progect in git

---
+++ script.js
@@ -0,0 +1,37 @@
+const productCard = document.querySelectorAll('.product__card-wrapper');
+const header = document.querySelector('.header__container-second');
+const headerBtn = document.querySelector('.burger')
+const headerSecond = document.querySelector('.header-second');
+
+
+
+
+productCard.forEach((element) => {
+    const btn = element.querySelector('.content__button');
+    element.addEventListener('mouseover', () => {
+        btn.style.opacity = '1';
+    });
+    element.addEventListener('mouseout', () => {
+        btn.style.opacity = '0';
+    });    
+});
+
+headerBtn.addEventListener('click', () => {
+    headerBtn.classList.toggle('open');
+    headerSecond.classList.toggle('active');
+});
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const burger = document.getElementById('burger');
+//     const mobileMenu = document.getElementById('mobileMenu');
+    
+//     burger.addEventListener('click', function() {
+//         this.classList.toggle('active');
+//         mobileMenu.classList.toggle('open');
+//     });
+// });
